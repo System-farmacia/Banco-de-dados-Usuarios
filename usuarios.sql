09eac20210bef9133fb0cdfb0340913fb0d95cc9
Davi-dev1 <davigancalveesdasilva@gmail.com>
2026-03-02 09:19:13 -0300

    Enviando modificações na database do projeto
    Signed-off-by: Davi-dev1 <davigancalveesdasilva@gmail.com>

--- usuarios.sql
+++ usuarios.sql
@@ -6,10 +6,14 @@ usuario_id int auto_increment primary key,
 nome_usuario varchar(25),
 CPF int unique,
 endereco_usuario varchar(100) not null unique,
-peso float 
+peso float ,
+email_usuario varchar(25) not null unique,
+senha_usuario varchar(12) not null unique,
+data_de_nasc date
 );
 
-insert into Usuarios(nome_usuario,CPF,endereco_usuario,peso) values('Marcos',1223343,' Rua das Alamedas 12',85.3);
-insert into Usuarios(nome_usuario,CPF,endereco_usuario,peso) values('Matheus ',45645678,' Rua Santo Amaro 4',77.8);
+
+insert into Usuarios(nome_usuario,CPF,endereco_usuario,peso,email_usuario,senha_usuario,data_de_nasc) values('Marcos',1223343,' Rua das Alamedas 12',85.3,'marcos@gmail.com','1234567','1985-02-09');
+insert into Usuarios(nome_usuario,CPF,endereco_usuario,peso,email_usuario,senha_usuario,data_de_nasc) values('Matheus ',45645678,' Rua Santo Amaro 4',77.8,'matheus@gmail.com','13156','2000-09-12');
 
 select * from Usuarios;
